dc60a17a23388d10b45985d658e71ba261dbff77
BSuria <Basuria@hotmail.com>
2025-02-15 06:44:09 -0500

    Changes

---
+++ server.js
@@ -0,0 +1,49 @@
+const http = require( "node:http" ),
+    fs   = require( "node:fs" ),
+    // IMPORTANT: you must run `npm install` in the directory for this assignment
+    // to install the mime library if you're testing this on your local machine.
+    // However, Glitch will install it automatically by looking in your package.json
+    // file.
+    mime = require( "mime" ),
+    dir  = "public/",
+    port = 3000
+
+const server = http.createServer( function( request,response ) {
+    if( request.method === "GET" ) {
+        handleGet( request, response )
+    }})
+
+const handleGet = function( request, response ) {
+    const filename = dir + request.url.slice( 1 )
+
+    if( request.url === "/" || request.url === "/index.html") {
+        let html = fs.readFileSync(__dirname + '/public/index.html','utf8');
+        response.end(html);
+    }else{
+        sendFile( response, filename )
+    }
+}
+
+const sendFile = function( response, filename ) {
+    const type = mime.getType( filename )
+
+    fs.readFile( filename, function( err, content ) {
+
+        // if the error = null, then we've loaded the file successfully
+        if( err === null ) {
+
+            // status code: https://httpstatuses.com
+            response.writeHeader( 200, { "Content-Type": type })
+            response.end( content )
+
+        } else {
+
+            // file not found, error code 404
+            response.writeHeader( 404 )
+            response.end( "404 Error: File Not Found" )
+
+        }
+    })
+}
+
+server.listen( process.env.PORT || port )
